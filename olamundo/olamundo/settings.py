cb90a33a4de0a7355101083fee5825863a847166
fagner <faa.louis@hotmail.com>
2022-10-12 22:23:32 -0300

    vai dar

--- olamundo/olamundo/settings.py
+++ olamundo/olamundo/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'olamundo.urls'
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'olamundo.wsgi.application'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 # Database
